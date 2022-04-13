1d0b2af6f17fc6f9b0759b6d9ffdce5fc4f5a748
Kalle Chen <kallechen.tw@gmail.com>
2022-04-13 10:29:37 +0800

    Add lint

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import signer from "node-signpdf";
-import { plainAddPlaceholder } from "node-signpdf/dist/helpers/index.js";
-import fs from "fs";
-import path from "path";
+import fs from 'fs'
+import signer from 'node-signpdf'
+import { plainAddPlaceholder } from 'node-signpdf/dist/helpers/index.js'
+import path from 'path'
 
-const pdfPath = "assets/sample.pdf";
-const certPath = "assets/keyStore.p12";
+const pdfPath = 'assets/sample.pdf'
+const certPath = 'assets/keyStore.p12'
 
 const main = async () => {
-  const pdfBuffer = fs.readFileSync(pdfPath);
-  console.log(pdfBuffer);
-  const certBuffer = fs.readFileSync(certPath);
+  const pdfBuffer = fs.readFileSync(pdfPath)
+  const certBuffer = fs.readFileSync(certPath)
   const inputBuffer = plainAddPlaceholder({
     pdfBuffer,
-  });
-  const signedPdf = signer.sign(inputBuffer, certBuffer);
-  fs.writeFileSync(path.join(__dirname, "./signed.pdf"), signedPdf);
-};
+  })
+  const signedPdf = signer.sign(inputBuffer, certBuffer)
+  fs.writeFileSync(path.join(__dirname, './signed.pdf'), signedPdf)
+}
 
-main();
+main()
